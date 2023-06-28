1b57a2a8866fd1fd90b4972687a11762cae99e1f
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-06-28 08:30:37 +0400

    Feature(auto-update): add manifest for updater

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import './styles.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
