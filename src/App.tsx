819492e9b8fb005b77932c46f1e7d368cbd5ec17
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-06-28 11:54:11 +0400

    Fix:signature refresh test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  checkUpdate,
-  installUpdate,
-  onUpdaterEvent
-} from '@tauri-apps/api/updater';
-import { relaunch } from '@tauri-apps/api/process';
+
 import './App.css';
 // import TopNav from './components/header/TopNav';
 
@@ -42,47 +37,6 @@ const data = [
 ];
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const checkForUpdates = async () => {
-      console.log('Checking for updates...');
-      const unlisten = await onUpdaterEvent(({ error, status }) => {
-        // This will log all updater events, including status updates and errors.
-        console.log('Updater event', error, status);
-      });
-
-      try {
-        // const manifestFetch = await fetch(
-        //   'https://raw.githubusercontent.com/Emmanuel97423/babone/master/update-manifest.json'
-        // );
-        // const manifestJson = await manifestFetch.json();
-        // console.log('Fetched manifest:', manifestJson);
-        const { shouldUpdate, manifest } = await checkUpdate();
-        console.log('manifest:', manifest);
-        console.log('shouldUpdate:', shouldUpdate);
-
-        if (shouldUpdate) {
-          // You could show a dialog asking the user if they want to install the update here.
-          console.log(
-            `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-          );
-
-          // Install the update. This will also restart the app on Windows!
-          await installUpdate();
-
-          // On macOS and Linux you will need to restart the app manually.
-          // You could use this step to display another confirmation dialog.
-          // await relaunch();
-        }
-      } catch (error) {
-        console.error(error);
-      }
-
-      // you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
-      unlisten();
-    };
-
-    checkForUpdates();
-  }, []);
   const Card: React.FC<ButtonProps> = ({ title, description, icon, link }) => {
     return (
       <div className="shadow-2xl">
