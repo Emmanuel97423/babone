ece180401d6f12d889e47c0002aab7bac041ea12
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-06-28 09:33:22 +0400

    Hotfix(updater): add row manifest link to Tauri config

--- src/App.tsx
+++ src/App.tsx
@@ -43,35 +43,45 @@ const data = [
 
 const App: React.FC = () => {
   useEffect(() => {
-    let unlisten: Function;
-
     const checkForUpdates = async () => {
-      unlisten = await onUpdaterEvent(({ error, status }) => {
+      console.log('Checking for updates...');
+      const unlisten = await onUpdaterEvent(({ error, status }) => {
+        // This will log all updater events, including status updates and errors.
         console.log('Updater event', error, status);
       });
 
       try {
+        // const manifestFetch = await fetch(
+        //   'https://raw.githubusercontent.com/Emmanuel97423/babone/master/update-manifest.json'
+        // );
+        // const manifestJson = await manifestFetch.json();
+        // console.log('Fetched manifest:', manifestJson);
         const { shouldUpdate, manifest } = await checkUpdate();
+        console.log('manifest:', manifest);
+        console.log('shouldUpdate:', shouldUpdate);
 
         if (shouldUpdate) {
+          // You could show a dialog asking the user if they want to install the update here.
           console.log(
             `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
           );
+
+          // Install the update. This will also restart the app on Windows!
           await installUpdate();
+
+          // On macOS and Linux you will need to restart the app manually.
+          // You could use this step to display another confirmation dialog.
           await relaunch();
         }
       } catch (error) {
         console.error(error);
       }
 
+      // you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
       unlisten();
     };
-    checkForUpdates();
 
-    // cleanup function
-    return () => {
-      unlisten && unlisten();
-    };
+    checkForUpdates();
   }, []);
   const Card: React.FC<ButtonProps> = ({ title, description, icon, link }) => {
     return (
